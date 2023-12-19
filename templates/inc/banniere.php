4586158741718f8f6993364d4b4af55c52acdc96
Maskeeded <maskeeded@gmail.com>
2023-12-19 14:07:33 +0100

    Update file paths and fix image references

--- templates/inc/banniere.php
+++ templates/inc/banniere.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>header</title>
-    <link rel="stylesheet" href="../../public/style.css">
-</head>
-<body>
+
+
 
     <div id="header-container">
-        <img src="../../public/img/banniere.jpg" id="header-image" alt="">
+
+        <img src="img/banniere.jpg" id="header-image" alt="">
+
         <div id="header-title">Vienne</div>
+
     </div>
 
-
-    
-</body>
-</html>
